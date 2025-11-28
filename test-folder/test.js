81f554492b9ad98ca7cb4459db9ba47bbd6a72b4
Isaiah Tshabalala <lakeside.junction.1984@gmail.com>
2025-11-28 17:13:34 +0200

    Added new function hasAll()

--- test-folder/test.js
+++ test-folder/test.js
@@ -6,6 +6,11 @@ Date        Version  Description
 2025/11/20  1.0.2    Corrected test code.
 */
 const commonFunctions = require('some-common-functions-js');
+/**Note to the developer: LOCAL TESTING INSTRUCTIONS:
+ * To "publish" your package locally, run "npm link" from the root folder of this project,
+ * then run "npm link some-common-functions-js" from test-folder.
+ * Then, run "node test.js" from test-folder to execute this test file.
+ */
 
 let anObject = {
     firstName: "Isaiah",
@@ -112,6 +117,19 @@ console.log("----END OF getPaths test----");
 
 console.log(commonFunctions.hasOnly(anObject, "firstName", "lastName", "address.houseNum",
                                     "address.streetName", "address.city", "address.country.name", "address.country.code"));
+                                    // false, because because anObject has address.mainPlace and address.subPlace which are extra to the specified fields.
+console.log("----END OF hasOnly test----");
+
+console.log(commonFunctions.hasAll(anObject, "firstName", "lastName", "address.houseNum",
+                                    "address.streetName", "address.city", "address.country.name", "address.country.code"));
+                                    // true, because anObject has all the specified fields, and more (allowed)
+console.log("----END OF hasAll test----");
+
+console.log(commonFunctions.hasOnlyAll(anObject, "firstName", "lastName", "address.houseNum",
+                                    "address.streetName", "address.city", "address.country.name", "address.country.code"));
+                                    // false, because anObject has address.mainPlace and address.subPlace which are extra to the specified fields.
+
+console.log("----END OF hasOnlyAll test----");                             
 
 let myArray = [100, 101, 102, 103, 104, 105, 106, 107];  
 console.log(commonFunctions.binarySearch(myArray, 103, 4)); // 4
@@ -119,15 +137,15 @@ let result = commonFunctions.compare(103, myArray[4]); // -1, meaning sought val
 console.log(result);
 console.log("----END OF binarySearch test----");
 
-let x = "Fong Kong";  
+let x = "Jiāng Fāng";  
 let y = "Isaiah Tshabalala";  
 result = commonFunctions.compare(x, y);
-console.log(result); // -1 because "Fong Kong" is before "Isaiah Tshabalala" in ascending order.
+console.log(result); // -1 because "Jiāng Fāng" is before "Isaiah Tshabalala" in ascending order.
 result = commonFunctions.compare(x, y, 'desc');
-console.log(result); // 1 because "Fong Kong" is after "Isaiah Tshabalala" in descending order.
+console.log(result); // 1 because "Jiāng Fāng" is after "Isaiah Tshabalala" in descending order.
 
 result = commonFunctions.compare(y, x);
-console.log(result); // 1 because "Isaiah Tshabalala" is after "Fong Kong" in ascending order.
+console.log(result); // 1 because "Isaiah Tshabalala" is after "Jiāng Fāng" in ascending order.
 console.log("----END OF compare test----");
 
 let teamsArray = [
